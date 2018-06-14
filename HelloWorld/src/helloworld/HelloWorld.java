240716565ae396a311b0828293a57af40ed490a3
Amalamol Raju <33655795+araju55@users.noreply.github.com>
2018-06-14 17:53:50 +0100

    Add files via upload

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Joseph
+ */
+public class HelloWorld extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+
+        Label label = new Label("Hello World!");
+
+        Scene scene = new Scene(label);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
